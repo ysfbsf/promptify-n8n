959edab1f4362037e4f24848e2f5220a64a3826f
SALMAN <elfadili.salman@gmail.com>
2023-10-16 11:42:39 +0100

    promptify node

--- credentials/PromptifyCredentialsApi.credentials.ts
+++ credentials/PromptifyCredentialsApi.credentials.ts
@@ -5,27 +5,19 @@ import {
 	INodeProperties,
 } from 'n8n-workflow';
 
-export class ExampleCredentialsApi implements ICredentialType {
-	name = 'exampleCredentialsApi';
-	displayName = 'Example Credentials API';
+export class PromptifyCredentialsApi implements ICredentialType {
+	name = 'promptifyApi';
+	displayName = 'Promptify API Token';
 	properties: INodeProperties[] = [
 		// The credentials to get from user and save encrypted.
 		// Properties can be defined exactly in the same way
 		// as node properties.
 		{
-			displayName: 'User Name',
-			name: 'username',
+			displayName: 'API Token',
+			name: 'apiToken',
 			type: 'string',
 			default: '',
-		},
-		{
-			displayName: 'Password',
-			name: 'password',
-			type: 'string',
-			typeOptions: {
-				password: true,
-			},
-			default: '',
+			typeOptions: { password: true },
 		},
 	];
 
@@ -35,13 +27,11 @@ export class ExampleCredentialsApi implements ICredentialType {
 	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
-			auth: {
-				username: '={{ $credentials.username }}',
-				password: '={{ $credentials.password }}',
-			},
 			qs: {
-				// Send this as part of the query string
-				n8n: 'rocks',
+				'api_token': '={{$credentials.apiToken}}'
+			},
+			headers: {
+				Authorization: '={{"Token " + $credentials.apiToken}}',
 			},
 		},
 	};
@@ -49,8 +39,8 @@ export class ExampleCredentialsApi implements ICredentialType {
 	// The block below tells how this credential can be tested
 	test: ICredentialTestRequest = {
 		request: {
-			baseURL: 'https://example.com/',
-			url: '',
+			baseURL: 'https://promptify.adtitan.io/api/meta',
+			url: '/templates/me',
 		},
 	};
 }
---
+++ nodes/PromptifyNode/PromptifyNode.node.ts
@@ -0,0 +1,159 @@
+import {
+	INodeType,
+	INodeTypeDescription,
+	IExecuteFunctions,
+	INodeExecutionData,
+} from 'n8n-workflow';
+import { EventSourceMessage, fetchEventSource } from 'fetch-event-source-hperrin';
+import { getTemplates, getInputs } from './GenericFunctions';
+import { Templates } from './types';
+
+export class PromptifyNode implements INodeType {
+
+	description: INodeTypeDescription = {
+		displayName: 'Promptify',
+		name: 'promptify',
+		group: ['transform'],
+		version: 1,
+		description: 'Basic Promptify Node',
+		// icon: 'file:promptify.svg',
+		defaults: {
+			name: 'Promptify',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'promptifyApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Templates',
+				name: 'template',
+				type: 'options',
+				default: '',
+				placeholder: 'Choose a template',
+				description: 'Choose a template and start generating',
+				typeOptions: {
+					loadOptionsMethod: 'getTemplates'
+				},
+				required: true
+			},
+			{
+				displayName: 'Inputs',
+				name: 'inputs',
+				type: 'resourceMapper',
+				noDataExpression: true,
+				default: {
+					mappingMode: 'defineBelow',
+					value: null,
+				},
+				required: true,
+				typeOptions: {
+					loadOptionsDependsOn: ['template'],
+					resourceMapper: {
+						resourceMapperMethod: 'getInputs',
+						mode: 'update',
+						fieldWords: {
+							singular: 'input',
+							plural: 'inputs',
+						},
+						addAllFields: true,
+						multiKeyMatch: true,
+					},
+				},
+			},
+		],
+	};
+
+
+	methods = {
+		loadOptions: {
+			getTemplates
+		},
+		resourceMapping: {
+			getInputs
+		},
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const templateId = this.getNodeParameter('template', 0) as string;
+		const inputs = this.getNodeParameter('inputs', 0) as any;
+    let generatedContent: string = "";
+
+		const options = {
+			method: "GET",
+			headers: {
+				"x-lf-source": "n8n"
+			},
+			uri: `https://promptify.adtitan.io/api/meta/templates/${templateId}`,
+			json: true
+		};
+		const template: Templates = await this.helpers.requestWithAuthentication.call(this, 'promptifyApi', options);
+		const inputsData = template.prompts?.map(prompt => ({
+			prompt: prompt.id,
+			contextual_overrides: [],
+			prompt_params: inputs.value
+		}))
+
+		const credentials = await this.getCredentials('promptifyApi')
+
+		await fetchEventSource(`https://promptify.adtitan.io/api/meta/templates/${templateId}/execute/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${credentials.apiToken as string}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(inputsData),
+			onopen: async (res: any) => {
+				console.log('open',res)
+			},
+			onmessage: (event :EventSourceMessage) => {
+				console.log(event)
+					try {
+						const parseData = JSON.parse(event.data.replace(/'/g, '"'));
+						const message = parseData.message;
+
+						if (event.event === "infer" && event.data) {
+								generatedContent += message || "";
+						} else {
+								if (message === "[C OMPLETED]" || message === "[COMPLETED]") {
+									console.log("[COMPLETED]: ",generatedContent)
+								}
+
+								if (message === "[INITIALIZING]") {
+									console.log("INITIALIZING")
+								}
+
+								if (message.includes("[ERROR]")) {
+									console.error("ERROR", message)
+								}
+						}
+					} catch {
+						console.error(event);
+					}
+			},
+			onerror: (err) => {
+				console.error(err)
+			},
+			onclose:() => {
+				console.error('close')
+			},
+		})
+
+		const promptifyGenerated = {
+			template: {
+				slug: template.slug,
+				title: template.title,
+				description: template.description,
+			},
+			content: generatedContent
+		}
+		return this.prepareOutputData([{
+			json: promptifyGenerated
+		}]);
+	}
+}
